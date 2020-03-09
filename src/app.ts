c62ea8378ab0a9bb4bd8d252cf17f1b0491b7941
kieni177 <franz.kienegger@gmail.com>
2020-03-09 20:24:25 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,40 +3,228 @@ import { Pool } from 'pg';
 import bodyParser from "body-parser";
 
 
+class RequestTable {
+    id: number;
+    correct: boolean;
+    equation: string;
+    max_moves: number;
+};
+
+class SolutionTable {
+    id: number;
+    equation: string;
+    moves: number;
+    user_supplied: boolean;
+    request_id: number; // (FK)
+};
+
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
-    password: 'admin',
-    port: 5432,
+    password: 'postgres',
+    port: 5433,
 });
 const app = express();
-const port = 3000;
+const port = 8080;
 app.use(bodyParser.json())
 
-app.get('/', async (req, res) => {
+
+
+class Verify_Request {
+
+    equation: string;
+    max_moves: number;
+    solutions: [];
+};
+
+class Verify_Solutions {
+    equation: string;
+    moves: number;
+}
+
+
+app.post('/puzzles/insert', async (req, res) => {
     try {
-        const now = await pool.query('SELECT * from NOW()');
+    
+        const queryText = "INSERT INTO Request(id, equation) VALUES($1, $2) RETURNING ID;";
+        pool.query(queryText, [1,'1=2']);
+
+      //const now = await pool.query('SELECT * from NOW()');
+
+        res.send("test");
+    } catch (err) {
+        console.log(err.stack)
+    }
+
+    
+});
+
+
+app.post('/puzzles/verify', async (req, res)  => {
+    try {
+        let verifyRequest = new Verify_Request();
+        verifyRequest.equation = req.body.equation;
+        verifyRequest.max_moves = req.body.max_moves;
+        verifyRequest.solutions = req.body.solutions;
+
+
+        let splitSymbols = verifyRequest.equation.split("");
+        let possibleSolutions: Map<String,Number> = new Map();
+
+        let currentSoltions: string[][] = [splitSymbols];
+
+        for (let m=0; m <= verifyRequest.max_moves; m++  ) {
+
+            const lastSolutions = [...currentSoltions];
+            currentSoltions = []
+
+            for(let p = 0; p <= lastSolutions.length; p++) {
+
+                for(let i = 0; i <= lastSolutions[p].length; i++) {
+
+                    //2b Schelife wenn ziffer -> m = moveDigit[]
+                    // m durcgehen (o)
+                    // -) c3 = copy symbole
+                    // -) c3[i] = m[0]
+
+
+                    if( splitSymbols[i] !==  '='  && splitSymbols[i] !==  '+' && splitSymbols[i] !==  '-') {
+                        let removeArray = removeDigit(splitSymbols[i]);
+
+                        if(removeArray && removeArray.length > 0 ) {
+                            for(let k = 0; k <= removeArray.length; k++) {
+                                let c = [...splitSymbols];
+                                c[i] = removeArray[k];
+
+
+                                for(let j = 0; j <= c.length; j++) {
+                                    if( i!=j ) {
+                                        if( splitSymbols[i] !==  '='  && splitSymbols[i] !==  '+' && splitSymbols[i] !==  '-') {
+                                            let addingArray = addingDigit(splitSymbols[i]);
+
+                                            if(addingArray && removeArray.length > 0  ) {
+
+                                                for(let a = 0;  a <= addingArray.length; a++) {
+                                                    let c2 = [...c];
+                                                    c2[j] = addingArray[a];
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                                //3. Schleife(j) alle Symbole // i!=j wenn ziffer ==> addingDigit
+                                //4. Schleife(a) a durcgehen (l)
+                                // c2 = copy c
+                                // c2[j] = a[l]
+                            }
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        //status 417
+        //status 200
+
+        res.send("hias");
+    } catch (err) {
+        console.log(err.stack)
+    }
+});
+
+
+
+
+function removeDigit(digit: string): string[] {
+    if(digit === '6')
+        return ['5'];
+
+     if(digit === '7')
+        return ['1'];
+
+    if(digit === '8')
+        return ['0','9', '6'];
+
+    if(digit === '9')
+        return ['3','5'];
+}
+
+function addingDigit(digit: string): string[] {
+    if(digit === '0')
+        return ['8'];
+
+    if(digit === '1')
+        return ['7'];
+
+    if(digit === '3')
+        return ['9'];
+
+    if(digit === '5')
+        return ['6','9'];
+
+    if(digit === '6')
+        return ['8'];
+
+    if(digit === '9')
+        return ['8'];
+}
+
+function movingDigit(digit: string): string[] {
+    if(digit === '0')
+        return ['6,9'];
+
+     if(digit === '2')
+        return ['3'];
+
+    if(digit === '5')
+        return ['3'];
+
+    if(digit === '6')
+        return ['9','0'];
+
+    
+    if(digit === '6')
+        return ['6','0'];
+}
+
+
+
+
+class Response_GET {
+    id: number;
+    correct: boolean;
+    equation: string;
+    max_moves: number;
+    user_provided: SolutionTable[];
+    pc_provided: SolutionTable[];
+}
+
+
+app.get('/puzzles/:id', async  (req, res) => {
+    try {
+        let request = await pool.query('SELECT * from REQUEST where id = ' + req.params.id  );
+        let solutionsUser = await pool.query('SELECT * from SOLUTION where user_supplied = true and request_id =' + req.params.id + ' ;');
+        let solutionsPC = await pool.query('SELECT * from SOLUTION where user_supplied = false and request_id =' + req.params.id + ' ;');
+
+
+        // set up Response
+        let response = new Response_GET();
+
+        response.id = request.body.rows[0].id;
+        response.correct   = request.body.rows[0].correct;
+        response.equation  = request.body.rows[0].equation;
+        response.max_moves = request.body.rows[0].max_moves;
+
+        response.user_provided = solutionsUser.body.rows;
+        response.pc_provided   = solutionsPC.body.rows;
+
         res.status(200);
-        res.send(now.rows[0].now);
-    } catch (err) {
-        console.log(err.stack)
-    }
-});
-
-app.get('/person', async (req, res)  => {
-    try {
-        const person = await pool.query('SELECT * from PERSON');
-        res.send(person.rows);
-    } catch (err) {
-        console.log(err.stack)
-    }
-});
-
-app.get('/person/:id', async (req, res)  => {
-    try {
-        const person = await pool.query('SELECT * from PERSON where id = ' + req.params.id  );
-        res.send(person.rows);
+        
+        res.send(response);
     } catch (err) {
         console.log(err.stack)
     }
